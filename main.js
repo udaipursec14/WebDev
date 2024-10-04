8cf5e1a2aba40ca842dc4c962c12fb39462e4f5d
Technoglobe <technoglobeudaipur@gmail.com>
2024-10-04 12:34:33 +0530

    Push

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ let div = document.querySelector('div');
 let head = document.querySelector('h1');
 let btn = document.querySelector('button');
 
+btn.addEventListener('click', ()=>{
+    console.log('Hello');
+})
